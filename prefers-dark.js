5528625747ed80b77cd53d369411fa87dfe92cc5
Navith <28162694+JakeNavith@users.noreply.github.com>
2020-08-30 17:10:29 -0400

    Fix linting errors (semicolons)

--- prefers-dark.js
+++ prefers-dark.js
@@ -1,22 +1,22 @@
 function checkDarkMode() {
-  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
 }
 
 function watchDarkMode() {
-  if (!window.matchMedia) return;
+  if (!window.matchMedia) return
 
-  window.matchMedia('(prefers-color-scheme: dark)').addListener(addDarkModeSelector);
+  window.matchMedia('(prefers-color-scheme: dark)').addListener(addDarkModeSelector)
 }
 
 function addDarkModeSelector() {
   if (checkDarkMode()) {
     // use your theme's class below
-    document.documentElement.classList.add('theme-dark');
+    document.documentElement.classList.add('theme-dark')
   } else {
     // use your theme's class below
-    document.documentElement.classList.remove('theme-dark');
+    document.documentElement.classList.remove('theme-dark')
   }
 }
 
-addDarkModeSelector();
-watchDarkMode();
+addDarkModeSelector()
+watchDarkMode()
