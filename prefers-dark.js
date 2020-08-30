7c76928c75f8a0ecb3111fe8ce06805839686141
Navith <28162694+JakeNavith@users.noreply.github.com>
2020-08-30 16:22:30 -0400

    Watch for changes to dark mode
    This is modeled after https://github.com/ChanceArthur/tailwindcss-dark-mode/pull/34

--- prefers-dark.js
+++ prefers-dark.js
@@ -1,11 +1,22 @@
-function prefersDark() {
-  return !!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+function checkDarkMode() {
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
 
-if (prefersDark()) {
-  // use your theme's class below
-  document.documentElement.classList.add('theme-dark')
-} else {
-  // use your theme's class below
-  document.documentElement.classList.remove('theme-dark')
+function watchDarkMode() {
+  if (!window.matchMedia) return;
+
+  window.matchMedia('(prefers-color-scheme: dark)').addListener(addDarkModeSelector);
 }
+
+function addDarkModeSelector() {
+  if (checkDarkMode()) {
+    // use your theme's class below
+    document.documentElement.classList.add('theme-dark');
+  } else {
+    // use your theme's class below
+    document.documentElement.classList.remove('theme-dark');
+  }
+}
+
+addDarkModeSelector();
+watchDarkMode();
